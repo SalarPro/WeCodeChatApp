a3fedaa127e7f7d32c9ea525f7cfc32f92643613
Salar Pro <salarkhalid97@gmail.com>
2022-01-09 18:03:28 +0300

    6:03

--- lib/src/data_model/message_model.dart
+++ lib/src/data_model/message_model.dart
@@ -2,6 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   String? message;
+  
 
-  Message({this.message});
+  Message(
+      {
+      this.message,
+      });
+
+  // from map which reads the data from the database
+
+  factory Message.fromMap(Map<String, dynamic> json) => Message(
+      
+      message: json["message"],
+      );
+
+  // toMap()
+  Map<String, dynamic> toMap() => {
+        
+        "message": message,
+      };
 }
--- lib/src/home_screen/home_screen_view.dart
+++ lib/src/home_screen/home_screen_view.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wecode2/src/custom_view/custome_view.dart';
+import 'package:wecode2/src/data_model/message_model.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -20,10 +22,37 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Row(
                 children: [
+                  FutureBuilder<QuerySnapshot>(
+                    future:
+                        FirebaseFirestore.instance.collection('messages').get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('error');
+                      } else if (!snapshot.hasData || snapshot.data == null) {
+                        return Text('empty');
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.done) {
+                        List<DocumentSnapshot> _docs = snapshot.data!.docs;
+
+                        List<Message> _users = _docs
+                            .map((e) => Message.fromMap(
+                                e.data() as Map<String, dynamic>))
+                            .toList();
+
+                        return ListView.builder(
+                            itemCount: _users.length,
+                            itemBuilder: (context, index) {
+                              return Text(_users[index].message ?? 'no message');
+                            });
+                      }
+                      return LinearProgressIndicator();
+                    },
+                  ),
+                  
                   TextFormField(
                     controller: _messageController,
-                    decoration:
-                        CustomView.ganeralInputDecoration(labelText: "labelText"),
+                    decoration: CustomView.ganeralInputDecoration(
+                        labelText: "labelText"),
                     validator: (value) {
                       if (value!.isEmpty) {
                         return 'the phone number is required';
@@ -31,19 +60,25 @@ class _HomeScreenState extends State<HomeScreen> {
                         return null;
                     },
                   ),
-
                   ElevatedButton.icon(
-                  onPressed: () async {
-                    setState(() {
-                      
-                    });
-                  },
-                  icon: Icon(
-                    Icons.login,
-                  ),
-                  label: Text(""),
-                ),
+                    onPressed: () async {
+                      setState(() {
 
+                        if  (_messageController.value.text != ""){
+                              await FirebaseFirestore.instance
+                                .collection('users')
+
+
+                        }
+
+
+                      });
+                    },
+                    icon: Icon(
+                      Icons.login,
+                    ),
+                    label: Text(""),
+                  ),
                 ],
               )
             ],
