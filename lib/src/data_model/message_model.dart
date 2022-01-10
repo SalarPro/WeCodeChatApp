3c449c0226fc588c7b2973307cd1247144558876
Salar Pro <salarkhalid97@gmail.com>
2022-01-11 00:24:56 +0300

    Creating message buble for user and partner, Message Model (username, message, created date, Timestemp conversion)

--- lib/src/data_model/message_model.dart
+++ lib/src/data_model/message_model.dart
@@ -2,23 +2,23 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   String? message;
-  
+  Timestamp? createdAt;
+  String? username;
 
-  Message(
-      {
-      this.message,
-      });
+  Message({this.message, this.createdAt, this.username});
 
   // from map which reads the data from the database
 
   factory Message.fromMap(Map<String, dynamic> json) => Message(
-      
-      message: json["message"],
+        message: json["message"],
+        createdAt: json["createdAt"],
+        username: json["username"],
       );
 
   // toMap()
   Map<String, dynamic> toMap() => {
-        
         "message": message,
+        "createdAt": createdAt,
+        "username": username,
       };
 }
